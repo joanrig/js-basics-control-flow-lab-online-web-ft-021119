dfea804f0317a6bbaab98212da72b6e622c23cbf
joanrig <github email address>
2019-05-22 19:50:55 -0400

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  let result
+  if (someValue < 400) {
+    result = "This one is on me!";
+  }
+  else if (2000 < someValue && someValue < 2500) {
+    result = 'I will gladly take your thirty bucks.';
+  }
+  else if  (someValue > 2500) {
+    result = 'No can do.';
+  }
+  return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  return(city === "NYC" ? "Ok, sounds good." : "No go.")
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip){
+  let result;
+  switch (tip) {
+    case 'generous':
+      result = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      result = 'Thank you.';
+      break;
+    default:
+      result = 'Bye.';
+      break;
+    }
+  return result;
 }
